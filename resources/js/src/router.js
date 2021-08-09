ec1a7119c3c65ee72d6faf1b7e1f34821441c071
Admin <isaacbest.ig@gmail.com>
2021-08-09 21:03:59 +0700

    up code

--- resources/js/src/router.js
+++ resources/js/src/router.js
@@ -10,7 +10,20 @@ const router = new VueRouter({
 	routes: [
 		{
 			path: '/',
-			component: () => import('./views/client/Index.vue')
+			component: () => import('./views/client/Index.vue'),
+			redirect: '/home',
+			children: [
+				{
+					path: 'home',
+					name: 'home',
+					component: () => import('./views/client/home/Home.vue')
+				},
+                {
+                    path: 'information/edit',
+                    name: 'user-edit',
+                    component: () => import('./views/client/user_information/UserEditInformation.vue')
+                },
+			]
 		},
 
 		{
@@ -27,6 +40,7 @@ const router = new VueRouter({
 		{
 			path: '/admin',
 			name: 'admin',
+
 			component: () => import('./views/admin/Index.vue'),
 			children: [
 				{
@@ -34,11 +48,23 @@ const router = new VueRouter({
 					name: 'admin-dashboard',
 					component: () => import('./views/admin/dashboard/Dashboard.vue')
 				},
+				{
+					path: 'flights',
+					name: 'admin-flights',
+					component: () => import('./views/admin/flights/Flights.vue')
+				},
+				{
+						path: 'airport',
+						name: 'admin-airport',
+						component: () => import('./views/admin/airports/Airports.vue')
+				},
                 {
-                    path: 'airport',
-                    name: 'admin-airport',
-                    component: () => import('./views/admin/airports/Airports.vue')
+                    path: 'user',
+                    name: 'admin-user',
+                    component: () => import('./views/admin/user/User.vue')
                 },
+
+
 			]
 		},
 
