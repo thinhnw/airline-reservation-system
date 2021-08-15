afa29f9500058cdcc5f48a1c61b9c5db63e07c03
anhnguyet2305 <sonbnnth2007009@fpt.edu.vn>
2021-08-15 10:51:29 +0700

    update

---
+++ resources/js/src/views/client/myAccount/EditMyAccount.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <b-container>
+      <b-col>
+        <label class="labelClose" for="checkboxEditInfor"><i class="far fa-times-circle"></i></label>
+        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+          <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+            <b-form-input
+                id="input-2"
+                v-model="form.name"
+                placeholder="Enter your name !"
+                required
+            ></b-form-input>
+          </b-form-group>
+          <b-form-group
+              id="input-group-1"
+              label="Phone number:"
+              label-for="input-1"
+              description="We'll never share your phone number with anyone else."
+          >
+            <vue-tel-input v-model="form.phone"></vue-tel-input>
+          </b-form-group>
+          <b-form-group
+              id="input-group-1"
+              label="Email address:"
+              label-for="input-1"
+              description="We'll never share your email with anyone else."
+          >
+            <b-form-input
+                id="input-1"
+                v-model="form.email"
+                type="email"
+                placeholder="Enter your email !"
+                required
+            ></b-form-input>
+          </b-form-group>
+
+          <b-button class="mr-1" type="submit" variant="primary">Change</b-button>
+          <b-button type="reset" variant="danger">Reset</b-button>
+        </b-form>
+      </b-col>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EditMyAccount",
+  data() {
+    return {
+      form: {
+        email: '',
+        name: '',
+        food: null,
+        checked: []
+      },
+      show: true
+    }
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault()
+    },
+    onReset(event) {
+      event.preventDefault()
+      // Reset our form values
+      this.form.email = ''
+      this.form.name = ''
+      this.form.phone=''
+      this.form.checked = []
+      // Trick to reset/clear native browser form validation state
+      this.show = false
+      this.$nextTick(() => {
+        this.show = true
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .labelClose{
+    text-align: right;
+    font-size: 22px;
+    color: red;
+    transform: translateX(30px);
+  }
+</style>
--- resources/js/src/views/client/myAccount/MyAccount.vue
+++ resources/js/src/views/client/myAccount/MyAccount.vue
@@ -1,9 +1,10 @@
 <template>
   <b-container>
+    <input type="checkbox" id="checkboxEditInfor" class="d-none">
     <b-card no-body>
       <b-tabs card>
         <b-tab title="Đặt chỗ của tôi !" active>
-          <b-card-text >
+          <b-card-text>
             <b-card bg-variant="dark" text-variant="white" title="vé máy bay khứ hồi">
               <b-card-text>
                 Bay từ thiên đường xuống địa ngục
@@ -11,7 +12,7 @@
               <b-button href="#" variant="warning">Chi tiết vé</b-button>
             </b-card>
           </b-card-text>
-<!--          else-->
+          <!--          else-->
           <div>
             <b-card no-body class="overflow-hidden p-4 ">
               <b-row no-gutters class="align-items-center">
@@ -19,7 +20,8 @@
                 <b-col>
                   <b-card-body title="Không tìm thấy đặt chỗ">
                     <b-card-text>
-                      Mọi chỗ bạn đặt sẽ được hiển thị tại đây. Hiện bạn chưa có bất kỳ đặt chỗ nào, hãy đặt trên trang chủ ngay!
+                      Mọi chỗ bạn đặt sẽ được hiển thị tại đây. Hiện bạn chưa có bất kỳ đặt chỗ nào, hãy đặt trên trang
+                      chủ ngay!
                     </b-card-text>
                     <b-link href="#" disabled>Đặt vé ngay !</b-link>
                   </b-card-body>
@@ -38,10 +40,10 @@
                   Bùi Sơn
                 </span>
               </div>
-              <b-button href="#" variant="warning">
+              <label for="checkboxEditInfor"  type="button" class="btn btn-warning" href="#" variant="warning">
                 <i class="black-icoin far fa-user-edit"></i>
                 Cập nhật hồ sơ !
-              </b-button>
+              </label>
             </b-card-text>
           </b-card>
           <b-card bg-variant="dark card-infor-user mt-1">
@@ -63,9 +65,9 @@
                   Số điện thoại :
                   0335950503
                 </span>
-                  <small>
-                    <p class="mt-2">Số di động để nhận tin báo liên quan đến tài khoản.</p>
-                  </small>
+                <small>
+                  <p class="mt-2">Số di động để nhận tin báo liên quan đến tài khoản.</p>
+                </small>
               </div>
             </b-card-text>
           </b-card>
@@ -77,8 +79,8 @@
                   Email :
                   knightstork.work@gmail.com
                 </span>
-                  <small>
-                    <p class="mt-2">Địa chỉ email để nhận tin báo liên quan đến tài khoản.</p>
+                <small>
+                  <p class="mt-2">Địa chỉ email để nhận tin báo liên quan đến tài khoản.</p>
                 </small>
               </div>
             </b-card-text>
@@ -86,31 +88,66 @@
         </b-tab>
       </b-tabs>
     </b-card>
+    <label class="layoutFormEdit" for="checkboxEditInfor"></label>
+    <div class="style-editMyAcc col-md-6 m-auto">
+      <EditMyAccount/>
+    </div>
   </b-container>
 </template>
 
 <script>
+import EditMyAccount from "@/components/myAccount/EditMyAccount";
 export default {
-name: "MyAccount"
+  name: "MyAccount",
+  components: {EditMyAccount}
 }
 </script>
 
 <style lang="scss" scoped>
-  .fa-exclamation-triangle{
-    font-size: 56px;
+.fa-exclamation-triangle {
+  font-size: 56px;
+  color: #ffc107;
+}
+
+.card-infor-user {
+  span, p {
+    color: #ffffff;
+    margin-bottom: 0;
+  }
+  i {
     color: #ffc107;
+    margin-right: 6px;
   }
-  .card-infor-user{
-    span,p{
-      color: #ffffff;
-      margin-bottom: 0;
-    }
-    i{
-      color: #ffc107;
-      margin-right: 6px;
-    }
-    .black-icoin{
-      color: #000000;
-    }
+
+  .black-icoin {
+    color: #000000;
   }
+}
+.style-editMyAcc{
+  background-color: #ffffff;
+  width: 500px;
+  border-radius: 15px;
+  padding: 22px 22px 30px 22px;
+  position: fixed;
+  top: 20%;
+  left: 33%;
+  transition: 0.3s;
+  display: none;
+}
+.layoutFormEdit{
+  position: fixed;
+  background-color: rgba(0,0,0, 0.5);
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  transition: 0.3s;
+  display: none;
+}
+#checkboxEditInfor:checked ~ .layoutFormEdit{
+  display: block;
+}
+#checkboxEditInfor:checked ~ .style-editMyAcc{
+  display: block;
+}
 </style>
