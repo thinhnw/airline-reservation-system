493d3089a9849137e89982a50c7a9145832b69e3
thinhnw <thinhnv0412@gmail.com>
2021-08-15 13:38:49 +0700

    Pg seed

--- database/seeders/AirportsPg.php
+++ database/seeders/AirportsPg.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class Airports extends Seeder
+class AirportsPg extends Seeder
 {
 		/**
 		 * Run the database seeds.
